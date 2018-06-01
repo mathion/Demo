e4d79d9f9b9737b7fbf6e5424e6e19d974b7f7e7
Ian Persson <mathion.mage@gmail.com>
2018-06-01 15:41:35 -0700

    Changed stuff

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
       for (int i = 0; i < 15; i++) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Seattle!");
       }
     }
 }
